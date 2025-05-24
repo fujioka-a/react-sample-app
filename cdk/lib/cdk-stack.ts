e33d193eec981a72600de1ee879a80ed3f37cf34
fujioka-a <atsushi3233@gmail.com>
2025-05-24 22:04:33 +0900

    OACを設定

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,59 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as cloudfront_origins from 'aws-cdk-lib/aws-cloudfront-origins';
 
 export class CdkStack extends cdk.Stack {
+  public readonly siteBucket: s3.Bucket;
+  public readonly distribution: cloudfront.Distribution;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    this.siteBucket = new s3.Bucket(this, 'SiteBucket', {
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'index.html',
+      publicReadAccess: false,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+      autoDeleteObjects: false,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // L2ディストリビューション作成（CfnDistributionからの移行)
+    this.distribution = new cloudfront.Distribution(this, 'SiteDistribution', {
+      defaultRootObject: 'index.html',
+      defaultBehavior: {
+        origin:
+          cloudfront_origins.S3BucketOrigin.withOriginAccessControl(
+            this.siteBucket
+          ),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD,
+      },
+    });
+
+    // S3のバケットポリシー追加
+    this.siteBucket.addToResourcePolicy(new iam.PolicyStatement({
+      actions: ['s3:GetObject'],
+      resources: [this.siteBucket.arnForObjects('*')],
+      principals: [
+        new iam.ServicePrincipal('cloudfront.amazonaws.com'),
+      ],
+      conditions: {
+        StringEquals: {
+          'AWS:SourceArn': `arn:aws:cloudfront::${this.account}:distribution/${this.distribution.distributionId}`,
+        },
+      },
+    }));
+
+    // 出力（CLIで値を使えるように）
+    new cdk.CfnOutput(this, 'BucketName', {
+      value: this.siteBucket.bucketName,
+    });
+
+    new cdk.CfnOutput(this, 'CloudFrontDomain', {
+      value: this.distribution.domainName,
+    });
   }
 }
